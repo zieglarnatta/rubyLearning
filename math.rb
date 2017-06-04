e952e1d562a653c56db14aaebc1c8168afd14c10
Roy Yap <roy.yap@welltok.com>
2017-06-04 01:27:53 -0700

    added more items in math.rb

--- math.rb
+++ math.rb
@@ -41,3 +41,36 @@ puts""
 puts "modulo / remainder 12 % 7"
 puts "Result: "
 puts "#{12%7}"
+
+#output formatting
+puts""
+puts""
+
+puts "assignment operations x = 12 and then x += 1 to add one"
+x=12
+puts "Result: "
+puts "#{x+=1}"
+
+#output formatting
+puts""
+puts""
+
+puts "assignment operations x-=1 to subtract 1"
+puts "Result: "
+puts "#{x-=1}"
+
+#output formatting
+puts""
+puts""
+
+puts "assignment operations x*=2 to multiply by 2"
+puts "Result: "
+puts "#{x*=2}"
+
+#output formatting
+puts""
+puts""
+
+puts "assignment operations x/=2 to divide by 2"
+puts "Result: "
+puts "#{x/=2}"
