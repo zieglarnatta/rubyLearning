5df54aedf35ba91dd2dbe39af779c9a4ffb7f522
Roy Yap <royyap1@gmail.com>
2019-09-16 07:02:05 -0700

    adding in sentenceReverse.rb

---
+++ sentenceReverser.rb
@@ -0,0 +1,18 @@
+#get input from user
+#parse each word into an entity in an Array
+#Make the 1st word and first alphabet become small
+#Make the last word & first alphabet get capitalized
+#parse the array and print out reverse order
+
+#Example: For instance, “Wherever you go, you can always find beauty.” would return as “Beauty find always, can you go you wherever.”
+
+
+class SentenceReverse
+  #get input from user
+  #parse each word into an entity in an Array
+  sentenceArray = ["",""]
+  #Make the 1st word and first alphabet become small
+  #Make the last word & first alphabet get capitalized
+  #parse the array and print out reverse order
+  puts sentenceArray
+  print
