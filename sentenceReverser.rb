c2f2aed379b11b76cc86820f2f08f6e45d9bf779
Roy Yap <zieglarnatta@users.noreply.github.com>
2019-09-16 22:25:49 -0700

    Update sentenceReverser.rb

--- sentenceReverser.rb
+++ sentenceReverser.rb
@@ -1,8 +1,18 @@
+#greet the user
+print "Welcome to the sentence reverser. Please input your sentence and my ruby code will parse it, put it into an array, and then rever the order of the sentence and capitalizing the first word in the resulting reversed sentence"
+
 #get input from user
+getSentence = gets
+
 #parse each word into an entity in an Array
+#for i = 1 till end, assign each word to the variable
+
 #Make the 1st word and first alphabet become small
 #Make the last word & first alphabet get capitalized
+#word (i) = word(i).[capitalize]
+
 #parse the array and print out reverse order
+#puts [printout all the reversed words]
 
 #Example: For instance, “Wherever you go, you can always find beauty.” would return as “Beauty find always, can you go you wherever.”
 
